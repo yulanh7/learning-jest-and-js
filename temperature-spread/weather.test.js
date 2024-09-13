2b2f0f09c4f98e7299dbbdaa08f880d129fd7596
Yulan Huang <yulanh7@gmail.com>
2024-09-13 15:38:19 +1000

    Add test for parsing data into object with correct headers

--- temperature-spread/weather.test.js
+++ temperature-spread/weather.test.js
@@ -1,4 +1,4 @@
-const { readDataFromFile } = require("./weather");
+const { readDataFromFile, parseHeaderAndData } = require("./weather");
 const fs = require('fs');
 
 jest.mock('fs');
@@ -15,5 +15,18 @@ test('should read data from the file', () => {
   expect(trimmedData).toEqual([
     '1948   1    8.9     3.3    ---     85.0    ---',
     '1948   2    7.9     2.2    ---     26.0    ---'
+  ])
+})
+
+test('should parse data into objects with correct headers', () => {
+  const dataLines = [
+    '1948   1    8.9     3.3    ---     85.0    ---',
+    '1948   2    7.9     2.2    ---     26.0    ---'
+  ]
+
+  const result = parseHeaderAndData(dataLines);
+  expect(result).toEqual([
+    { yyyy: 1948, mm: 1, tmax: 8.9, tmin: 3.3, af: null, rain: 85.0, sun: null },
+    { yyyy: 1948, mm: 2, tmax: 7.9, tmin: 2.2, af: null, rain: 26.0, sun: null }
   ])
 })
