7c2d97f7f20ba96f10caa6f2cbe6268435a4bec2
Yulan Huang <yulanh7@gmail.com>
2024-09-13 13:57:50 +1000

    add a test case to validate only lines starting with a year are processed by readDataFromFile

--- temperature-spread/weather.test.js
+++ temperature-spread/weather.test.js
@@ -4,4 +4,7 @@ test('should read data from the file', () => {
   const data = readDataFromFile("heathrow-weather-data.txt");
   expect(data).toBeTruthy();
   expect(data.length).toBeGreaterThan(0);
+  data.forEach(element => {
+    expect(/^\d{4}/.test(element.trim())).toBe(true);
+  });
 })
