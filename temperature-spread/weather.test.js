8fc8bdb8ca09dee7fdcf0da8fd5ede5e27674002
Yulan Huang <yulanh7@gmail.com>
2024-09-13 15:09:33 +1000

    Refactor tests to use mock data instead of read file for readDataFromFile function

--- temperature-spread/weather.test.js
+++ temperature-spread/weather.test.js
@@ -1,10 +1,17 @@
 const { readDataFromFile } = require("./weather");
+const fs = require('fs');
+
+jest.mock('fs');
 
 test('should read data from the file', () => {
-  const data = readDataFromFile("heathrow-weather-data.txt");
-  expect(data).toBeTruthy();
-  expect(data.length).toBeGreaterThan(0);
-  data.forEach(element => {
-    expect(/^\d{4}/.test(element.trim())).toBe(true);
-  });
+  const mockData = `
+  1948   1    8.9     3.3    ---     85.0    ---
+  1948   2    7.9     2.2    ---     26.0    ---`;
+
+  fs.readFileSync.mockReturnValue(mockData);
+  const weatherData = readDataFromFile('fakePath');
+  expect(weatherData).toEqual([
+    '1948   1    8.9     3.3    ---     85.0    ---',
+    '1948   2    7.9     2.2    ---     26.0    ---'
+  ])
 })
